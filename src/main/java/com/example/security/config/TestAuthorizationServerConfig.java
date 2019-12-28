f2b0fbe68f400c00f012e0fde5751271d785cf30
ex-yinhongchan001 <ex-yinhongchan001@ocft.com>
2019-12-28 12:54:43 +0800

    update pom

--- src/main/java/com/example/security/config/TestAuthorizationServerConfig.java
+++ src/main/java/com/example/security/config/TestAuthorizationServerConfig.java
@@ -1,26 +1,10 @@
 package com.example.security.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @Configuration
 @EnableAuthorizationServer
-public class TestAuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+public class TestAuthorizationServerConfig  {
 
-    @Override
-    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory()
-                .withClient("self")
-                .secret(passwordEncoder.encode("selfSecret"))
-                .redirectUris("http://example.com")
-                .authorizedGrantTypes("refresh_token", "authorization_code", "password")
-                .scopes("all");
-    }
-
-    }
+}
--- src/main/java/com/example/security/service/TestUserDetailsService.java
+++ src/main/java/com/example/security/service/TestUserDetailsService.java
@@ -2,6 +2,7 @@ package com.example.security.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,6 +10,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 这个类实际的作用是如果依赖了spring security 输入的用户名的密码的校验
@@ -22,6 +26,8 @@ public class TestUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
         log.info("当前登陆用户是:{}", userName);
-        return new User(userName, passwordEncoder.encode("111111"), AuthorityUtils.commaSeparatedStringToAuthorityList("admin, ROLE_USER"));
+        //此处不加ROLE_USER会报403 Access_Deniedd的错误
+        return new User(userName, passwordEncoder.encode("111111"),
+                AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_ADMIN, ROLE_USER"));
     }
 }
